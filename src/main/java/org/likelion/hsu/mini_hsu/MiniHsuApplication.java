a12409246f16f5d155c536915588cb941b033f01
WeonJae <kongwonjae123@naver.com>
2025-06-01 16:36:32 +0900

    최종본

---
+++ src/main/java/org/likelion/hsu/mini_hsu/MiniHsuApplication.java
@@ -0,0 +1,13 @@
+package org.likelion.hsu.mini_hsu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniHsuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniHsuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/likelion/hsu/mini_hsu/MiniHsuApplicationTests.java
@@ -0,0 +1,13 @@
+package org.likelion.hsu.mini_hsu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniHsuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
